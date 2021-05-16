bbe651d1fadec37eab49c86d557230c381a7a31f
txnight <81194644+txnight@users.noreply.github.com>
2021-05-16 18:05:34 -0400

    Update and rename tabla_multiplicar to tabla_multiplicar.py

--- tabla_multiplicar.py
+++ tabla_multiplicar.py
@@ -1,7 +1,7 @@
      
 contador=1
 numero=int(input("ingrese el numero a multiplicar "))
-while contador<=10:
-    x=numero*contador
+while contador<=10: 
+    x=numero*contador 
     print("{} X {} = {}".format(numero,contador,x))
     contador=contador+1
